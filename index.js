71a000228faf8522f45a9c3154696249b9d8352f
Kanishka Jayabalan <kanishkajayabalan@example.com>
2026-05-22 11:21:42 +0530

    Configure Wrangler to serve static assets including index.html

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 export default {
-  async fetch(request) {
-    return new Response('Moonlit Aura', { status: 200 });
+  async fetch(request, env) {
+    const url = new URL(request.url);
+    
+    // If path is root, serve index.html
+    if (url.pathname === '/') {
+      url.pathname = '/index.html';
+    }
+    
+    // Serve static assets
+    return env.ASSETS.fetch(url.toString());
   }
 };
